5157eb9ab242db86e90909ed33362dd6233cef06
johnnylord <johnnylord7410@gmail.com>
2020-11-07 23:24:02 +0800

    Change model from fc to cnn

--- model/mnist.py
+++ model/mnist.py
@@ -8,43 +8,50 @@ class VAEMNIST(nn.Module):
     def __init__(self, latent_dim):
         super().__init__()
         self.encoder = nn.Sequential(
-                            nn.Linear(784, 128),
+                            # (1, 28, 28)
+                            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+                            nn.BatchNorm2d(32),
                             nn.ReLU(inplace=True),
-                            nn.Linear(128, 32),
-                            nn.ReLU(inplace=True))
-
-        self.mu = nn.Linear(32, latent_dim)
-        self.log_var = nn.Linear(32, latent_dim)
-        self.upsample = nn.Linear(latent_dim, 32)
+                            nn.MaxPool2d(2, 2),
+                            # (32, 14, 14)
+                            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+                            nn.BatchNorm2d(64),
+                            nn.ReLU(inplace=True),
+                            nn.MaxPool2d(2, 2))
+                            # (64, 7, 7)
 
         self.decoder = nn.Sequential(
-                            nn.Linear(32, 128),
+                            # (64, 7, 7)
+                            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2),
+                            nn.BatchNorm2d(32),
                             nn.ReLU(inplace=True),
-                            nn.Linear(128, 784),
+                            # (32, 15, 15)
+                            nn.ConvTranspose2d(32, 1, kernel_size=3, stride=2),
                             nn.Sigmoid())
+                            # (1, 30, 30)
 
-    def forward(self, x):
-        # Flatten input
+        self.mu = nn.Linear(64*7*7, latent_dim)
+        self.log_var = nn.Linear(64*7*7, latent_dim)
+        self.upsample = nn.Linear(latent_dim, 64*7*7)
+
+    def forward(self, imgs):
+        x = self.encoder(imgs)
         x = x.view(x.size(0), -1)
 
-        # Encoder
-        x = self.encoder(x)
-
-        # Latent
         mu = self.mu(x)
         log_var = self.log_var(x)
         z = self._sampling(mu, log_var)
 
-        # Decoder
         x = self.upsample(z)
+        x = x.view(x.size(0), 64, 7, 7)
         x = self.decoder(x)
 
-        x = x.view(x.size(0), 1, 28, 28)
-        return x, mu, log_var
+        return x[:, :, 1:29, 1:29], mu, log_var
 
-    def get_latent(self, x):
+    def get_latent(self, imgs):
+        x = self.encoder(imgs)
         x = x.view(x.size(0), -1)
-        x = self.encoder(x)
+
         mu = self.mu(x)
         log_var = self.log_var(x)
         z = self._sampling(mu, log_var)
@@ -62,40 +69,54 @@ class CVAEMNIST(nn.Module):
     def __init__(self, latent_dim):
         super().__init__()
         self.encoder = nn.Sequential(
-                            nn.Linear(784+CVAEMNIST.N_CLASSES, 128),
+                            # (1, 28, 28)
+                            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+                            nn.BatchNorm2d(32),
                             nn.ReLU(inplace=True),
-                            nn.Linear(128, 32),
-                            nn.ReLU(inplace=True))
-
-        self.mu = nn.Linear(32, latent_dim)
-        self.log_var = nn.Linear(32, latent_dim)
-        self.upsample = nn.Linear(latent_dim+CVAEMNIST.N_CLASSES, 32)
+                            nn.MaxPool2d(2, 2),
+                            # (32, 14, 14)
+                            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+                            nn.BatchNorm2d(64),
+                            nn.ReLU(inplace=True),
+                            nn.MaxPool2d(2, 2))
+                            # (64, 7, 7)
 
         self.decoder = nn.Sequential(
-                            nn.Linear(32, 128),
+                            # (64, 7, 7)
+                            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2),
+                            nn.BatchNorm2d(32),
                             nn.ReLU(inplace=True),
-                            nn.Linear(128, 784),
+                            # (32, 15, 15)
+                            nn.ConvTranspose2d(32, 1, kernel_size=3, stride=2),
                             nn.Sigmoid())
+                            # (1, 30, 30)
+
+        self.mu = nn.Linear(64*7*7+CVAEMNIST.N_CLASSES, latent_dim)
+        self.log_var = nn.Linear(64*7*7+CVAEMNIST.N_CLASSES, latent_dim)
+        self.upsample = nn.Linear(latent_dim+CVAEMNIST.N_CLASSES, 64*7*7)
 
     def forward(self, imgs, labels):
+        x = self.encoder(imgs)
+        x = x.view(x.size(0), -1)
+
         one_hots = one_hot(labels, CVAEMNIST.N_CLASSES).float()
-        x = torch.cat([imgs.view(imgs.size(0), -1), one_hots], axis=1)
-        x = self.encoder(x)
+        x = torch.cat([x, one_hots], axis=1)
 
         mu = self.mu(x)
         log_var = self.log_var(x)
         z = self._sampling(mu, log_var)
-
         x = torch.cat([z, one_hots], axis=1)
         x = self.upsample(x)
+        x = x.view(x.size(0), 64, 7, 7)
         x = self.decoder(x)
-        x = x.view(x.size(0), 1, 28, 28)
-        return x, mu, log_var
+
+        return x[:, :, 1:29, 1:29], mu, log_var
 
     def get_latent(self, imgs, labels):
+        x = self.encoder(imgs)
+        x = x.view(x.size(0), -1)
         one_hots = one_hot(labels, CVAEMNIST.N_CLASSES).float()
-        x = torch.cat([imgs.view(imgs.size(0), -1), one_hots], axis=1)
-        x = self.encoder(x)
+        x = torch.cat([x, one_hots], axis=1)
         mu = self.mu(x)
         log_var = self.log_var(x)
         z = self._sampling(mu, log_var)
